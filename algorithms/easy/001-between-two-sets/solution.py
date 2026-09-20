1051c8ee102f4ab9e5dbc13c398b8357c2a6f377
ShubhagamanSingh <singhshubhagaman@gmail.com>
2026-09-20 13:04:31 +0530

    Add challenge solution

--- algorithms/easy/001-between-two-sets/solution.py
+++ algorithms/easy/001-between-two-sets/solution.py
@@ -1,4 +1,24 @@
-# Between Two Sets
-# https://www.hackerrank.com/challenges/between-two-sets/problem
+def getTotalX(a, b):
+    # LCM of all elements in array a.
+    # Any valid number must be a multiple of this value.
+    lcm_a = reduce(lambda x, y: (x * y) // gcd(x, y), a)
 
-# Write your solution here.
+    # GCD of all elements in array b.
+    # Any valid number must divide this value.
+    gcd_b = reduce(gcd, b)
+
+    # If LCM(a) does not divide GCD(b),
+    # no number can satisfy both conditions.
+    if gcd_b % lcm_a != 0:
+        return 0
+
+    count = 0
+    multiple = lcm_a
+
+    # Check only multiples of LCM(a) up to GCD(b).
+    while multiple <= gcd_b:
+        if gcd_b % multiple == 0:
+            count += 1
+        multiple += lcm_a
+
+    return count    
